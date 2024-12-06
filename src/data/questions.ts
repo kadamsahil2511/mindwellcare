c4a99b11e6d1ee979f98566cab65d4936238f2b2
kadamsahil2511 <kadamsahil2511@gmail.com>
2024-12-07 01:58:23 +0530

    added questions

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -13,5 +13,50 @@ export const anxietyQuestions = [
     id: 3,
     text: "How often do you worry too much about different things?",
     options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 4,
+    text: "Little interest or pleasure in doing things",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 5,
+    text: "Feeling down, depressed, or hopeless",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 6,
+    text: "Trouble falling or staying asleep, or sleeping too much",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 7,
+    text: "Feeling tired or having little energy",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 8,
+    text: "Poor appetite or overeating",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 9,
+    text: "Feeling bad about yourself - or that you are a failure",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 10,
+    text: "Trouble concentrating on things, such as reading the newspaper or watching television",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 11,
+    text: "Moving or speaking so slowly that other people could have noticed. Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
+  },
+  {
+    id: 12,
+    text: "Thoughts that you would be better",
+    options: ["Not at all", "Several days", "More than half the days", "Nearly every day"]
   }
 ];
